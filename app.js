c7f83549cf4e258a844d985c6c876820c711ab21
Gabrielviscio10 <gonzalo.viscio@icloud.com>
2024-07-30 16:25:56 +0000

    casi terminado

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 // Esta es mi función que suma dos números
-const sum = (a,b) => {
-    return a + b
+const sum = (a, b) => {
+    return a + b;
 }
 
 // Solo un registro en consola para nosotros
-console.log(sum(7,3))
+console.log(sum(7, 3));
 
 // Exporta la función para usarla en otros archivos 
 // (similar a la palabra clave "export" cuando se usa webpack)
 module.exports = { sum };
 
-
-
 // Tasas de conversión
-let oneEuroIs = {
+const oneEuroIs = {
     "JPY": 156.5, // japan yen
     "USD": 1.07, // us dollar
     "GBP": 0.87, // british pound
@@ -21,18 +19,21 @@ let oneEuroIs = {
 
 function fromDollarToYen(dollars) {
     let euros = dollars / oneEuroIs["USD"]; // Convertir dólares a euros
-    let yen = euros * oneEuroIs["JPY"];     // Convertir euros a yenes
+    let yen = Math.floor(euros * oneEuroIs["JPY"]); // Convertir euros a yenes
     return yen;
 }
 
 function fromEuroToDollar(euros) {
-    return euros * oneEuroIs["USD"];
+    let dollars = Math.floor(euros * oneEuroIs["USD"]);
+    return dollars;
 }
 
 function fromYenToPound(yen) {
-    let euros = yen / oneEuroIs["JPY"];    // Convertir yenes a euros
-    let pounds = euros * oneEuroIs["GBP"]; // Convertir euros a libras
+    let euros = yen / oneEuroIs["JPY"]; // Convertir yenes a euros
+    let pounds = Math.floor(euros * oneEuroIs["GBP"]); // Convertir euros a libras
     return pounds;
 }
-
-module.exports = { fromDollarToYen, fromEuroToDollar, fromYenToPound, oneEuroIs };
+console.log(fromDollarToYen(10)),
+ console.log(fromEuroToDollar(5)),
+ console.log(fromYenToPound(1000)),
+module.exports = { sum, fromDollarToYen, fromEuroToDollar, fromYenToPound, oneEuroIs };
